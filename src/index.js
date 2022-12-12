f0c2d9410801dd6b2ff70342880fe98933b04aed
Chhatrapati Mandwe <mandwec12@gmail.com>
2022-12-12 23:30:18 +0530

    assignment 7

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser");
+const router = require('../routes/student');
+const mongoose = require('mongoose')
+const port = 8080
+// app.use(express.urlencoded());
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect("mongodb://localhost/RECORD",()=>{
+    console.log("connect to the server");
+})
+// app.use(bodyParser.json())
+// your code goes here
+app.use("/api/",router)
+
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+module.exports = app;   
